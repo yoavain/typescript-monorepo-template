8680c98490039820fa19e0a60366796c13472925
Yoav Vainrich <yoavain@gmail.com>
2025-08-16 15:30:55 +0300

    ts-config changes

--- fix-lockfile.config.js
+++ fix-lockfile.config.js
@@ -2,7 +2,7 @@ const { execSync } = require("node:child_process");
 const path = require("node:path");
 
 const lernaInfoOutput = execSync("lerna list --all --json", { encoding: "utf8" });
-const lernaPackages = JSON.parse(lernaInfoOutput).map(p => path.relative(__dirname, p.location));
+const lernaPackages = JSON.parse(lernaInfoOutput).map((p) => path.relative(__dirname, p.location));
 
 const config = {
     includePaths: [
